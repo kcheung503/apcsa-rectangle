2ff5035a5546f1e8f4b3985d66d3749990236821
Kyle Cheung <kcheung2@ucvts.tec.nj.us>
2019-10-01 09:19:30 -0400

    Final commit

--- apcsarectangle.java
+++ apcsarectangle.java
@@ -1,1 +1,17 @@
-aaa
+import java.util.Scanner;
+
+public class apcsarectangle {
+    public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+
+      System.out.print("Enter a length: ");
+      double length = in.nextDouble();
+      System.out.print("Enter a width: ");
+      double width = in.nextDouble();
+
+      double area = length * width;
+      System.out.println("The area of a " + length + "-by-" + width + " rectangle is " + area + ".");
+
+      in.close();
+    }
+}
